27d5e1b803cc45fc60b6db9fbc6c9ff1f30807e3
jossanCosta <jocostasan@gmail.com>
2023-08-31 11:16:59 -0300

    update

--- Qgis2threejs.js
+++ Qgis2threejs.js
@@ -29,7 +29,7 @@ Q3D.Config = {
   orthoCamera: false,
   viewpoint: {      // z-up
     default: {      // assumed that origin is (0, 0, 0) and base extent width in 3D world coordinates is 1
-      pos: new THREE.Vector3(0, -1, -3),
+      pos: new THREE.Vector3(0, -1, -20),
       lookAt: new THREE.Vector3()
     }
   },
